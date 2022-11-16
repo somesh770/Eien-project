811e648accf44796d34afa3291c56b46700c7155
admin <admin@DESKTOP-NJMF2GL>
2022-11-17 03:03:30 +0530

    rest assured

--- src/test/java/Practice02/GetMethod.java
+++ src/test/java/Practice02/GetMethod.java
@@ -1,43 +1,91 @@
 package Practice02;
 
-import org.testng.annotations.Test;
-import org.json.simple.JSONObject;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import com.google.gson.JsonObject;
+
+import io.restassured.http.ContentType;
+
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
-import java.util.concurrent.TimeUnit;
-
-import io.restassured.http.ContentType;
-import io.restassured.response.Response;
-
-
 public class GetMethod 
-{
+{	
+	
  @Test
  public void getMethod1()
  {
-	 baseURI = "https://reqres.in/api";
-	 
-	 given().
-	 header("Content-type","Appliction/json").contentType(ContentType.JSON).
-	 get("/users/23").then().statusCode(404).log().all();
-	 
+	 baseURI= "https://reqres.in/api/";
+		
+	given().
+	contentType(ContentType.JSON).
+	when().
+	get("users?page=2").
+	then().statusCode(200).
+	body("data[1].first_name",equalTo("Lindsay")).
+	log().all();
+		
  }
- 
-//----------------------------------------------------------------
+//---------------------------------------------------------------------- 
  @Test
- public void getMetohd2()
+ public void GetsingleUser()
  {
-	 baseURI ="https://reqres.in/api";
+	 baseURI= "https://reqres.in/api/";
+	 given().contentType(ContentType.JSON).
+	 when().
+	 get("users/2").
+	 then()
+	 .statusCode(200)
+	 .body("data.first_name",equalTo("Janet")).log().all();
+	 	
+
+ }
+//-------------------------------------------------------------------------- 
+ 
+ @Test
+ public void postMethod()
+ {
+	 JsonObject req = new JsonObject();
+	 req.put("name", "morpheus");
+	 req.put("job", "leader");
 	 
-	 given().header("content-type","Application/json").contentType(ContentType.JSON).
-	 get("/unknown").then().statusCode(200).log().all();
+	 baseURI = "https://reqres.in/api/";
+	 
+	  given()
+	 .contentType(ContentType.JSON)
+	 .body(req.toJSONString())
+	 .when()
+	 .post("users").then()
+	 .statusCode(201)
+	 .body("name", equalTo("morpheus"))
+	 .body("job", equalTo("leader"))
+	 .log().all();
+	  	 
+ }
+ @Test
+ public void putMethod()
+ {
+	 JSONPObject req = new JSONPObject();
+	 req.put("name", "morpheus");
+	 req.put("job", "zion resident");
+	 
+	 baseURI = "https://reqres.in/api/";
+	 
+	  given().auth().basic("username", "password")
+	 .contentType(ContentType.JSON)
+	 .body(req.toJSONString())
+	 .when()
+	 .put("users/2").then()
+	 .statusCode(200)
+	 .body("name", equalTo("morpheus"))
+	 .body("job", equalTo("zion resident"))
+	 .log().all();
  }
  
-//----------------------------------------------------------------
-
-
+ 
 }
+
